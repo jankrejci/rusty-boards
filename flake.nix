ce514b2a0ac779637b0e25d8b81235f647d9f527
jankrejci <krejcijan@protonmail.com>
2026-02-26 09:12:59 +0100

    rusty-boards: Add treefmt-nix formatter
    - add root flake with treefmt-nix for rustfmt and nixfmt
    - add formatting check via nix flake check

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Rusty Boards - ESP32-C3 boards with embedded Rust firmware";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        treefmtEval = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.rustfmt.enable = true;
+          programs.nixfmt.enable = true;
+        };
+      in
+      {
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatting = treefmtEval.config.build.check self;
+      }
+    );
+}
